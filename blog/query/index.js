15712af0b8eae2a1d027b61680f181b6d7dadc7d
OritVaras <levi.orit@gmail.com>
2023-10-17 11:15:53 -0700

    add basic files

---
+++ blog/query/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+const handleEvent = (type, data) => {
+    if(type === 'PostCreated'){
+        const {id,title} = data;
+        posts[id] = {id,title, comments:[]};
+    }
+    if(type === 'CommentCreated'){
+        const {id,content,status, postId} = data;
+
+        posts[postId].comments.push({id,content,status})
+    }
+    if(type === 'CommentUpdated'){
+        const {id,content,status, postId} = data;
+
+        const comments = posts[postId].comments;
+        const comment = comments.find(com=> com.id === id);
+        comment.status = status
+        comment.content = content;
+    }
+}
+
+app.get('/posts', (req,res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req,res) => {
+    const {type, data} = req.body;
+    handleEvent(type, data);
+    res.status(200).send({});
+});
+
+
+app.listen(4002, async ()=>{
+    console.log('Listenning on 4002');
+    try{
+        const res = await axios.get('http://localhost:4005/events');
+        for(let event of res.data){
+            console.log(`processing event ${event.type}`);
+            handleEvent(event.type, event.data);
+        }
+    }catch(err){
+        console.log(err)
+    }
+});
